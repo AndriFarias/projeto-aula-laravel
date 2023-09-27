c00ebd65e15b6cb834410d0a13bceedbf4e45041
AndriFarias <andrielly.farias21@gmail.com>
2023-09-26 23:03:28 -0400

    adicionando alguns comentarios

--- resources/views/site/usuario/index.blade.php
+++ resources/views/site/usuario/index.blade.php
@@ -1,35 +1,44 @@
-@extends('tema.site.index')
-@section('titulo', 'Lista de usuários')
+@extends('tema.site.index')  {{-- Estende o layout da página --}}
+@section('titulo', 'Lista de usuários')  {{-- Define o título da página --}}
+@section('conteudo')  {{-- Define o conteúdo da página --}}
 
-@section('conteudo')
-@vite(['resources/scss/app.scss', 'resources/js/app.js'])
-<x-nav-bar />
+    {{-- Inclui recursos Vite para SCSS e JS --}}
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 
-<h1 class="m-3">Lista de usuários</h1>
+    {{-- Inclui o componente de barra de navegação --}}
+    <x-nav-bar />
 
-<ol class="list-group list-group-numbered m-3">
-@foreach($usuarios as $usuario)
-<li class="list-group-item">Nome: {{ $usuario['nome'] }}, Telefone: {{ $usuario['telefone'] }}</li>
-@endforeach
-</ol>
+    {{-- Título da página --}}
+    <h1 class="m-3">Lista de usuários</h1>
 
-<ul class="list-group list-group-horizontal m-3">
+    {{-- Lista ordenada de usuários --}}
+    <ol class="list-group list-group-numbered m-3">
+        @foreach($usuarios as $usuario)
+            <li class="list-group-item">Nome: {{ $usuario['nome'] }}, Telefone: {{ $usuario['telefone'] }}</li>
+        @endforeach
+    </ol>
 
-@for ($i = 0; $i < 5; $i++)
-<li class="list-group-item">O número agora é {{ $i }}</li>
-@endfor
-</ul>
+    {{-- Lista horizontal de números --}}
+    <ul class="list-group list-group-horizontal m-3">
+        @for ($i = 0; $i < 5; $i++)
+            <li class="list-group-item">O número agora é {{ $i }}</li>
+        @endfor
+    </ul>
 
-@if (count($usuarios) === 1)
-<div class="alert alert-warning m-3" role="alert">Tem só um usuário!</div>
-@elseif (count($usuarios) > 1)
-<div class="alert alert-warning m-3" role="alert">Tem vários usuários!</div>
-@else
-<div class="alert alert-warning m-3" role="alert">Não tem nenhum usuário!</div>
-@endif
+    {{-- Condição para exibir alerta --}}
+    @if (count($usuarios) === 1)
+        <div class="alert alert-warning m-3" role="alert">Tem só um usuário!</div>
+    @elseif (count($usuarios) > 1)
+        <div class="alert alert-warning m-3" role="alert">Tem vários usuários!</div>
+    @else
+        <div class="alert alert-warning m-3" role="alert">Não tem nenhum usuário!</div>
+    @endif
 
-{{-- Comentário --}}
+ 
+    {{-- Exibe uma mensagem com base na condição de idade --}}
+    {{ $idade >= 18 ? 'Maior de idade.' : 'Menor de idade.' }}
 
-{{ $idade >= 18 ? 'Maior de idade.' : 'Menor de idade.' }}
+    
+
 
 @endsection
